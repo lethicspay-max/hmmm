2d1147ce10e2c41a8900edb71151cb6f03f8be10
lethicspay-max <lethicspay@gmail.com>
2026-01-04 19:39:30 +0530

    Updated Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -197,7 +197,7 @@ export function Navbar() {
               </Link>
               <Link
                 to="/faq"
-                className="block px-3 py-2 text-gray-700 hover:text-blue-600"
+                className="block px-3 py-2 text-gray-700 hover:text-red-600"
                 onClick={() => setIsMenuOpen(false)}
               >
                 FAQ
