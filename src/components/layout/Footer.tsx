db79fb5d4fc2fb68cc060c797b2b73ececb9cd8e
lethicspay-max <lethicspay@gmail.com>
2026-01-04 17:34:20 +0530

    Updated Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -64,7 +64,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-300">
-          <p>&copy; 2025 Tapwell. All rights reserved.</p>
+          <p>&copy; 2025 Suraj International. All rights reserved.</p>
         </div>
       </div>
     </footer>
