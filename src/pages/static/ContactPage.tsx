e720130335ad8a65235b676de9167e158dc93b8d
lethicspay-max <lethicspay@gmail.com>
2026-01-04 20:19:44 +0530

    Updated ContactPage.tsx

--- src/pages/static/ContactPage.tsx
+++ src/pages/static/ContactPage.tsx
@@ -301,7 +301,7 @@ Western express highway malad east,<br />
             
             <div className="mt-6 text-center">
               <a
-                href="mailto:rases@tapwell.co.in"
+                href="mailto:Prema.surajinternational@gmail.com"
                 className="text-red-600 hover:text-red-700 font-medium"
               >
                 View all FAQs →
